cb98cface46b171a4b2e533483c9a83482f898bd
Carlos Arellano Casale <40230544+casareafer2908@users.noreply.github.com>
2021-09-13 16:53:09 +0800

    refactor test
    AAA tests convention

--- test/service_random_number_test.dart
+++ test/service_random_number_test.dart
@@ -3,33 +3,20 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Random Number Service returns an interger', () {
+    //arrange
     final randomNumberService = RandomNumberService();
+    //act
     randomNumberService.randomNumberService();
+    //assert
     expect(randomNumberService.value is int, true);
   });
   test('Random Number Service returns an interger >= 0 && >= 10', () {
+    //arrange
     final randomNumberService = RandomNumberService();
+    //act
     randomNumberService.randomNumberService();
+    //assert
     expect(randomNumberService.value >= 0 && randomNumberService.value <= 10,
         true);
   });
 }
-
-/*
-*
-
-import 'package:test/test.dart';
-import 'package:counter_app/counter.dart';
-
-void main() {
-  test('Counter value should be incremented', () {
-    final counter = Counter();
-
-    counter.increment();
-
-    expect(counter.value, 1);
-  });
-}
-
-*
-* */
